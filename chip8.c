a0b96dd7db213b0a6d988b6c09eea67597eca229
Stephen Kang <stephenkang9@gmail.com>
2018-10-21 17:21:02 -0400

    Several opcodes implemented

--- chip8.c
+++ chip8.c
@@ -77,31 +77,35 @@ void cpu_cycle() {
   
   switch (cpu.opcode & 0xF000) {
 
-    case 0x0000:
+    // bless testing in C -- so much fun ... 
+    case 0x0000: // useless right now CLS, RET, SYS addr
       printf ("Opcode not implemented | %u", cpu.opcode);
+      break;
+    case 0x1000: // JP addr 0x1nnn
+      cpu.pc = cpu.opcode & 0x0FFF;
+      break;
+    case 0x2000: // CALL addr 0x2nnn
+      stack[cpu.sp] = cpu.pc;
+      sp += 1;
+      cpu.pc = cpu.opcode & 0x0FFF;
+      break;
+    case 0x3000: // SE Vx, byte
+      (register[(cpu.opcode & 0x0F00) >> 8] == (cpu.opcode & 0x00FF)) ? cpu.pc += 4 : cpu.pc += 2;
+      break;
+    case 0x4000: // SNE Vx, byte
+      (register[(cpu.opcode & 0x0F00) >> 8] != (cpu.opcode & 0x00FF)) ? cpu.pc += 4 : cpu.pc += 2;
       break; 
-    case 0x1000:
+    case 0x5000: // SE Vx. Vy
+      (register[(cpu.opcode & 0x0F00) >> 8] == register[(cpu.opcode & 0x00F0) >> 4]) ? cpu.pc += 4 : cpu.pc += 2;
+      break;
+    case 0x6000: // LD Vx, byte
+      register[(cpu.opcode & 0x0F00) >> 8] = (cpu.opcode & 0x00FF);
+      cpu.pc += 2;
+      break;
+    case 0x7000: // ADD
       printf ("Opcode not implemented | %u", cpu.opcode);
       break;
-    case 0x2000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
-      break;
-    case 0x3000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
-      break;
-    case 0x4000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
-      break;
-    case 0x5000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
-      break;
-    case 0x6000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
-      break;
-    case 0x7000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
-      break;
-    case 0x8000:
+    case 0x8000: // lots
       printf ("Opcode not implemented | %u", cpu.opcode);
       break;
     case 0x9000:
