8a4e50fc48183e91a85938330e96873321e2aac5
Stephen Kang <stephenkang9@gmail.com>
2018-11-04 12:25:43 -0500

    completed all opcodes

--- chip8.c
+++ chip8.c
@@ -14,7 +14,7 @@ typedef struct {
 
   unsigned char graphics[64 * 32]; // could use 2d array
 
-  unsigned char delay_timmer;
+  unsigned char delay_timer;
   unsigned char sound_timer;
 
   unsigned short stack[16];
@@ -22,6 +22,7 @@ typedef struct {
 
   unsigned char keyboard_inputs[16];
 
+  unsigned char draw;
 } CPU; 
 
 CPU cpu;
@@ -52,6 +53,7 @@ void initialize_cpu() {
   cpu.opcode          = 0;
   cpu.index_register  = 0;
   cpu.sp              = 0;
+  cpu.draw            = 0;
 
   // load font's into memory 0x050-0x0A0
   memcpy(&cpu.memory[0x050], &chip8_fontset[0x000], sizeof(chip8_fontset));
@@ -111,11 +113,45 @@ void cpu_cycle() {
       cpu.pc += 2; 
       break;
     case 0x8000: // lots -- will get to
-      printf ("Opcode not implemented | %u", cpu.opcode);
+      switch (cpu.opcode & 0x000F) {
+        case 0x0000: // LD Vx, Vy
+          register[(cpu.opcode & 0x0F00) >> 8] = register[(cpu.opcode & 0x00F0) >> 4];
+          break;
+        case 0x0001:
+          register[(cpu.opcode & 0x0F00) >> 8] |= register[(cpu.opcode & 0x00F0) >> 4];
+          break;
+        case 0x0002:
+          register[(cpu.opcode & 0x0F00) >> 8] &= register[(cpu.opcode & 0x00F0) >> 4];
+          break;
+        case 0x0003:
+          register[(cpu.opcode & 0x0F00) >> 8] ^= register[(cpu.opcode & 0x00F0) >> 4];
+          break;
+        case 0x0004:
+          int add = ((cpu.opcode & 0x0F00) >> 8) + ((cpu.opcode & 0x00F0) >> 4);
+          (add > 0xFFFF) ? register[0xF] = 1 : register[0xF] = 0;
+          register[(cpu.opcode & 0x0F00) >> 8] = add; 
+          break;
+        case 0x0005:
+          (((cpu.opcode & 0x0F00) >> 8) > ((cpu.opcode & 0x00F0) >> 4)) ? register[0xF] = 1 : register[0xF] = 0;
+          register[(cpu.opcode & 0x0F00) >> 8] -= register[(cpu.opcode & 0x00F0) >> 4];
+          break;
+        case 0x0006:
+          (register[(cpu.opcode & 0x0F00) >> 8] & 0x0001) ? register[0xF] = 1 : register[0xF] = 0;
+          register[(cpu.opcode & 0x0F00) >> 8] /= 2;
+          break;
+        case 0x0007:
+          (((cpu.opcode & 0x0F00) >> 8) < ((cpu.opcode & 0x00F0) >> 4)) ? register[0xF] = 1 : register[0xF] = 0;
+          register[(cpu.opcode & 0x0F00) >> 8] = register[(cpu.opcode & 0x00F0) >> 4] - register[(cpu.opcode & 0x0F00) >> 8];
+          break;
+        case 0x000E:
+          (register[(cpu.opcode & 0x0F00) >> 8] & 0x1000) ? register[0xF] = 1 : register[0xF] = 0;
+          register[(cpu.opcode & 0x0F00) >> 8] *= 2;
+          break;
+      }
+      cpu.pc += 2;
       break;
     case 0x9000: // SNE Vx, Vy
       (register[(cpu.opcode & 0x0F00) >> 8] != register[(cpu.opcode & 0x00F0) >> 4]) ? cpu.pc += 4 : cpu.pc += 2;
-      printf ("Opcode not implemented | %u", cpu.opcode);
       break;
     case 0xA000: // LD I, addr
       cpu.index_register = (cpu.opcode 0x0FFF);
@@ -129,13 +165,94 @@ void cpu_cycle() {
       cpu.pc += 2;
       break;
     case 0xD000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
+      unsigned short x = register[(cpu.opcode 0x0F00) >> 8];
+      unsigned short y = register[(cpu.opcode 0x00F0) >> 4];
+      unsigned short n = register[(cpu.opcode 0x000F)];
+
+      unsigned short p;
+      register[0xF] = 0;
+
+      for (int yy = 0; yy < h; yy++) {
+        p = cpu.memory[cpu.index_register + yy];
+        for (int xx = 0; xx < 8; xx++) {
+          if ((p & (0x80 >> xx)) != 0) {
+            if(graphics[(x + xx + ((y + yy) * 64))] == 1)
+              register[0xF] = 1;                                 
+            graphics[x + xx + ((y + yy) * 64)] ^= 1;
+          }
+        }
+      }
+      cpu.draw = 1;
+      cpu.pc += 2;
       break;
     case 0xE000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
+      switch (cpu.opcode & 0x00FF) {
+        case 0x009E:
+          if(keyboard_inputs[register[(cpu.opcode & 0x0F00) >> 8]] != 0)
+            cpu.pc += 4;
+          else
+            cpu.pc += 2;
+          break;
+        case 0x00A1:
+          if(keyboard_inputs[register[(cpu.opcode & 0x0F00) >> 8]] == 0)
+            cpu.pc += 4;
+          else
+            cpu.pc += 2;
+          break;
+      }
       break;
     case 0xF000:
-      printf ("Opcode not implemented | %u", cpu.opcode);
+      switch (cpu.opcode & 0x00FF) {
+        case 0x0007:
+          register[(cpu.opcode & 0x0F00) >> 8] = cpu.deplay_timer; 
+          cpu.pc += 2;
+          break;
+        case 0x000A:
+          for (int i = 0; i < 16; i++) {
+            if (cpu.keyboard_inputs[i] != 0) {
+              register[(cpu.opcode & 0x0F00) >> 8] = i;
+              cpu.pc += 2;
+              break;
+            }
+          }
+          break;
+        case 0x0015:
+          cpu.deplay_timer = register[(cpu.opcode & 0x0F00) >> 8];
+          cpu.pc += 2;
+          break;
+        case 0x0018:
+          cpu.sound_timer = register[(cpu.opcode & 0x0F00) >> 8];
+          cpu.pc += 2;
+          break;
+        case 0x001E:
+          cpu.index_register += register[(cpu.opcode & 0x0F00) >> 8];
+          cpu.pc += 2;
+          break;
+        case 0x0029:
+          cpu.index_register = register[(cpu.opcode & 0x0F00) >> 8] * 0x5;
+					cpu.pc += 2;
+          break;
+        case 0x0033:
+          cpu.memory[cpu.index_register]     = register[(cpu.opcode & 0x0F00) >> 8] / 100;
+					cpu.memory[cpu.index_register + 1] = (register[(cpu.opcode & 0x0F00) >> 8] / 10) % 10;
+					cpu.memory[cpu.index_register + 2] = (register[(cpu.opcode & 0x0F00) >> 8]) % 10;
+          cpu.pc += 2;
+          break;
+        case 0x0055:
+          for (int i = 0; i <= ((cpu.opcode & 0x0F00) >> 8); i++)
+						cpu.memory[cpu.index_register + i] = register[i];
+
+					cpu.index_register += ((cpu.opcode & 0x0F00) >> 8) + 1;
+					cpu.pc += 2;
+          break;
+        case 0x0065:
+          for (int i = 0; i <= ((cpu.opcode & 0x0F00) >> 8); i++)
+						register[i] = cpu.memory[cpu.index_register + i];
+
+					cpu.index_register += ((cpu.opcode & 0x0F00) >> 8) + 1;
+					cpu.pc += 2;
+          break;
+      }
       break;
     default:
       printf ("Unknown opcode -- yikes| %u", cpu.opcode);
@@ -147,6 +264,11 @@ int main() {
   
   initialize_cpu();
   load_rom("pong.rom");
+
+  // for loop
+  // if (cpu.draw)
+  //    draw -> set cpu.draw to 0
+  //
 
   return 0;
 }
